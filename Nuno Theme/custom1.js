42fc3e3608d42eaaa2c100cdca2af0d1f3300abe
conradoekoki <44292115+conradoekoki@users.noreply.github.com>
2019-05-09 10:06:23 +0100

    Add files via upload

---
+++ Nuno Theme/custom1.js
@@ -0,0 +1,33 @@
+/*======== TRANSPARENT NAVIGATION ======*/
+
+$(document).ready(function() {
+	$(window).scroll(function() {
+		if($(this).scrollTop() > 300) {
+			$('.navbar').addClass('solid');
+		} else {
+			$('.navbar').removeClass('solid');
+		}
+		});
+	});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
